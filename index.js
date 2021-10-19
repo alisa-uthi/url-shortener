48f762b920ad392f0efc9fbe642bd53d3b99ab87
kimberlinn99 <alisa.uth99@gmail.com>
2021-10-19 17:03:49 +0700

    adjust error handler middleware

--- index.js
+++ index.js
@@ -15,11 +15,18 @@ if (process.env.NODE_ENV !== "production") {
 }
 app.use(express.json());
 app.use(cors());
-app.use(middlewares.errorHandler);
 
 const urlShortenerRoute = require("./routes/url_shortener_route");
 
-app.use("/", urlShortenerRoute);
+app.use("/api/v1/", urlShortenerRoute);
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.use(middlewares.errorHandler);
 
 const PORT = process.env.PORT || 8080;
 
--- middlewares.js
+++ middlewares.js
@@ -1,5 +1,8 @@
 const errorHandler = (error, req, res, next) => {
-  res.status(error.status || 500).json({ error: error.message });
+  res.status(res.statusCode || 500).json({
+    error: error.message,
+    stack: process.env.NODE_ENV === "production" ? "stack" : error.stack,
+  });
 };
 
 module.exports = {
